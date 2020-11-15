f06c2bef2970328ea929a9524eb050d1d5f670ea
Deshan <deshanm123@gmail.com>
2020-11-15 18:38:09 +0530

    Health care Interface
    Built drop down function  list to parties requirements and multipanel layered panel architecture in a single window to open panel nterfaces

---
+++ HealthCareManagementSystem/src/healthcaremanagementsystem/HealthCareManagementSystem.java
@@ -0,0 +1,18 @@
+
+package healthcaremanagementsystem;
+
+/**
+ *
+ * @author Deshan
+ */
+public class HealthCareManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DashBoard dBoard = new DashBoard();
+        dBoard.setVisible(true);
+    }
+    
+}

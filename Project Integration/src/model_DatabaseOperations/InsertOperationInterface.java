b7324fe100079da0573f9bb457331c8b9a352cea
Deshan <deshanm123@gmail.com>
2020-11-29 22:15:55 +0530

    database integration

---
+++ Project Integration/src/model_DatabaseOperations/InsertOperationInterface.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model_DatabaseOperations;
+
+
+public interface InsertOperationInterface {
+    public void  insertAppoinment(String patientName,String patientType,String appointmentDate, String appointmentTime);
+}
